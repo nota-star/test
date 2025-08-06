0148f774cb2d6fadcdc7cafd9ca57b29471c6d1a
nota-star <105321120+nota-star@users.noreply.github.com>
2025-08-06 17:44:42 -0400

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,11 @@
+@echo off
+title Test Batch File
+echo ====================================
+echo         This is a test.bat
+echo ====================================
+
+echo Hello, world!
+echo The current directory is: %cd%
+echo The current date and time: %date% %time%
+
+pause
